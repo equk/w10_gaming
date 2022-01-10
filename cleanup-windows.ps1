cdaa7eadc3a16a24afc54d36703d77849b89bb45
equk <equk@users.noreply.github.com>
2022-01-10 15:24:54 +0000

    created new powershell script cleanup-windows

---
+++ cleanup-windows.ps1
@@ -0,0 +1,21 @@
+#requires -version 4.0
+#requires -runasadministrator
+
+<#
+.SYNOPSIS
+    Script to Cleanup Windows 10
+.DESCRIPTION
+    Script to Cleanup Windows 10
+    Requires Admin Access
+
+    Twitter: @equilibriumuk
+
+    Created for Windows 10 21H2
+.NOTES
+    File Name      : disable-optional.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V4
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>
