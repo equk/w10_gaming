f13e43f8a6701ed2c3b94dcf003b34dbac3a079f
equk <equk@users.noreply.github.com>
2022-01-10 15:26:01 +0000

    added lists of installed packages & features to remove

--- cleanup-windows.ps1
+++ cleanup-windows.ps1
@@ -19,3 +19,29 @@
     Script posted on github:
     https://github.com/equk
 #>
+
+$packages = @{
+    "Microsoft.BingWeather"
+    "Microsoft.Office.OneNote"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    "Microsoft.WindowsMaps"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+}
+
+$features = @(
+    "SMB1Protocol"
+    "WindowsMediaPlayer"
+)
+
+$optionals = @(
+    "App.StepsRecorder"
+    "Hello.Face.18967"
+    "MathRecognizer"
+    "Media.WindowsMediaPlayer"
+    "OneCoreUAP.OneSync"
+    "Print.Fax.Scan"
+)
